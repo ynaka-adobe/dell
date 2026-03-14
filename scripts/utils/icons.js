47546b9feec3daa66e923c3ca6b418d50939dff5
AEMY Studio <noreply@aemy.studio>
2026-03-14 18:31:09 +0000

    Add header, search blocks
    Automated migration updates generated by Experience Modernization Agent.
    
    Changed files (8):
    - .migration/project.json
    - blocks/header/header.css
    - blocks/search/search.css
    - blocks/search/search.js
    - img/icons/logo.svg
    - img/icons/search.svg
    - scripts/utils/icons.js
    - styles/styles.css

--- scripts/utils/icons.js
+++ scripts/utils/icons.js
@@ -1,14 +1,29 @@
 import { getConfig } from '../ak.js';
 
 const { codeBase } = getConfig();
+const svgCache = {};
+
+async function fetchSvg(name) {
+  if (!svgCache[name]) {
+    svgCache[name] = fetch(`${codeBase}/img/icons/${name}.svg`)
+      .then((resp) => (resp.ok ? resp.text() : ''))
+      .catch(() => '');
+  }
+  return svgCache[name];
+}
 
 export default function loadIcons(icons) {
   for (const icon of icons) {
     const name = icon.classList[1].substring(5);
-    const svg = `<svg class="${icon.className}">
-        <use href="${codeBase}/img/icons/${name}.svg#${name}"></use>
-    </svg>`;
-    icon.insertAdjacentHTML('afterend', svg);
-    icon.remove();
+    fetchSvg(name).then((svgText) => {
+      if (!svgText) return;
+      const temp = document.createElement('div');
+      temp.innerHTML = svgText;
+      const svg = temp.querySelector('svg');
+      if (!svg) return;
+      svg.setAttribute('class', icon.getAttribute('class'));
+      icon.insertAdjacentElement('afterend', svg);
+      icon.remove();
+    });
   }
 }
